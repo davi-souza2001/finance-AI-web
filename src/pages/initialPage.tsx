c976dee054aa50fa07abb1fe5bab47166a26688a
Davi Souza <davisouza2001dv@gmail.com>
2025-07-13 15:36:54 -0300

    first commit

---
+++ src/pages/initialPage.tsx
@@ -0,0 +1,9 @@
+export function InitialPage() {
+  return (
+    <div className="min-h-screen px-4 py-8">
+      <div className="mx-auto max-w-6xl">
+       <p>teste</p>
+      </div>
+    </div>
+  )
+}
