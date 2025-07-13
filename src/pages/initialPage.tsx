7e8d9ff38b0eb82b467a96d1d2cdeb06232fe78d
Davi Souza <davisouza2001dv@gmail.com>
2025-07-13 16:04:21 -0300

    feat: add table

--- src/pages/initialPage.tsx
+++ src/pages/initialPage.tsx
@@ -1,8 +1,27 @@
+import { columns, type Payment } from "@/components/payments/columns";
+import { DataTable } from "@/components/payments/data-table";
+
 export function InitialPage() {
+  const data: Payment[] = [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    {
+      id: "489e1d42",
+      amount: 125,
+      status: "processing",
+      email: "example@gmail.com",
+    },
+  ];
+
   return (
     <div className="min-h-screen px-4 py-8">
       <div className="mx-auto max-w-6xl">
-       <p>teste</p>
+        <p>teste</p>
+        <DataTable columns={columns} data={data} />
       </div>
     </div>
   )
