6a6db1a558f6c8dc32599cd6e1c40eabc96f3c65
Davi Souza <davisouza2001dv@gmail.com>
2025-07-14 21:30:26 -0300

    feat: add cards

--- src/components/section-card.tsx
+++ src/components/section-card.tsx
@@ -10,31 +10,29 @@ import {
   CardTitle,
 } from '@/components/ui/card'
 
-export function SectionCards() {
+export function SectionCard() {
   return (
-    <div className='grid @5xl/main:grid-cols-4 @xl/main:grid-cols-2 grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card *:data-[slot=card]:shadow-xs lg:px-6 dark:*:data-[slot=card]:bg-card'>
-      <Card className="@container/card">
-        <CardHeader>
-          <CardDescription>Total Revenue</CardDescription>
-          <CardTitle className='font-semibold @[250px]/card:text-3xl text-2xl tabular-nums'>
-            $1,250.00
-          </CardTitle>
-          <CardAction>
-            <Badge variant="outline">
-              <IconTrendingUp />
-              +12.5%
-            </Badge>
-          </CardAction>
-        </CardHeader>
-        <CardFooter className="flex-col items-start gap-1.5 text-sm">
-          <div className="line-clamp-1 flex gap-2 font-medium">
-            Trending up this month <IconTrendingUp className="size-4" />
-          </div>
-          <div className="text-muted-foreground">
-            Visitors for the last 6 months
-          </div>
-        </CardFooter>
-      </Card>
-    </div>
+    <Card className="@container/card w-96">
+      <CardHeader>
+        <CardDescription>Total Revenue</CardDescription>
+        <CardTitle className='font-semibold @[250px]/card:text-3xl text-2xl tabular-nums'>
+          $1,250.00
+        </CardTitle>
+        <CardAction>
+          <Badge variant="outline">
+            <IconTrendingUp />
+            +12.5%
+          </Badge>
+        </CardAction>
+      </CardHeader>
+      <CardFooter className="flex-col items-start gap-1.5 text-sm">
+        <div className="line-clamp-1 flex gap-2 font-medium">
+          Trending up this month <IconTrendingUp className="size-4" />
+        </div>
+        <div className="text-muted-foreground">
+          Visitors for the last 6 months
+        </div>
+      </CardFooter>
+    </Card>
   )
 }
--- src/pages/initial-page.tsx
+++ src/pages/initial-page.tsx
@@ -1,28 +1,35 @@
-import { columns, type Payment } from "@/components/payments/columns";
-import { DataTable } from "@/components/payments/data-table";
-import { SectionCards } from "@/components/section-card";
+import { columns, type Payment } from '@/components/payments/columns'
+import { DataTable } from '@/components/payments/data-table'
+import { SectionCard } from '@/components/section-card'
 
 export function InitialPage() {
   const data: Payment[] = [
     {
-      id: "728ed52f",
+      id: '728ed52f',
       amount: 100,
-      status: "pending",
-      email: "m@example.com",
+      status: 'pending',
+      description: 'm@example.com',
+      name: 'John Doe',
+      category: 'Food',
     },
     {
-      id: "489e1d42",
+      id: '489e1d42',
       amount: 125,
-      status: "processing",
-      email: "example@gmail.com",
+      status: 'processing',
+      description: 'example@gmail.com',
+      name: 'Jane Doe',
+      category: 'Food',
     },
-  ];
+  ]
 
   return (
     <div className="min-h-screen px-4 py-8">
-      <div className="mx-auto max-w-6xl">
-        <p>teste</p>
-        <SectionCards />
+      <div className='mx-auto flex max-w-6xl flex-col items-start justify-start gap-4'>
+        <div className="flex w-full items-center justify-start gap-4">
+          <SectionCard />
+          <SectionCard />
+          <SectionCard />
+        </div>
         <DataTable columns={columns} data={data} />
       </div>
     </div>
