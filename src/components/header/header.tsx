62132c210c78e83069f086924a6b16501a223f58
Davi Souza <davisouza2001dv@gmail.com>
2025-07-16 21:46:25 -0300

    feat: add header and tabs

---
+++ src/components/header/header.tsx
@@ -0,0 +1,41 @@
+import { ArrowDownIcon, LogOutIcon } from 'lucide-react'
+import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
+import { Button } from '../ui/button'
+import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover'
+
+export const Header = () => {
+  return (
+    <div className="flex w-full items-center justify-between">
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button
+            className="flex cursor-pointer items-center gap-3"
+            variant="ghost"
+          >
+            <Avatar>
+              <AvatarImage alt="@shadcn" src="https://github.com/shadcn.png" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            <p>Davi Souza</p>
+            <ArrowDownIcon className="h-4 w-4" />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-28">
+          <Button
+            className="cursor-pointer hover:bg-destructive/50"
+            variant="destructive"
+          >
+            <LogOutIcon />
+            Sair
+          </Button>
+        </PopoverContent>
+      </Popover>
+      <Button
+        className="cursor-pointer text-zinc-500 hover:bg-zinc-100 hover:text-zinc-400"
+        variant="ghost"
+      >
+        Help
+      </Button>
+    </div>
+  )
+}
--- src/pages/initial-page.tsx
+++ src/pages/initial-page.tsx
@@ -1,6 +1,8 @@
+import { Header } from '@/components/header'
 import { columns, type Payment } from '@/components/payments/columns'
 import { DataTable } from '@/components/payments/data-table'
 import { SectionCard } from '@/components/section-card'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 
 export function InitialPage() {
   const data: Payment[] = [
@@ -25,12 +27,30 @@ export function InitialPage() {
   return (
     <div className="min-h-screen px-4 py-8">
       <div className='mx-auto flex max-w-6xl flex-col items-start justify-start gap-4'>
-        <div className="flex w-full items-center justify-start gap-4">
-          <SectionCard />
-          <SectionCard />
-          <SectionCard />
-        </div>
-        <DataTable columns={columns} data={data} />
+      <Header />
+        <Tabs defaultValue="finances">
+          <TabsList className='mb-5 w-full'>
+            <TabsTrigger value="finances">Finances</TabsTrigger>
+            <TabsTrigger value="investment">Investment</TabsTrigger>
+          </TabsList>
+          <TabsContent value="finances">
+            <div className='mx-auto flex max-w-6xl flex-col items-start justify-start gap-4'>
+              <div className="flex w-full items-center justify-start gap-4">
+                <SectionCard />
+                <SectionCard />
+                <SectionCard />
+              </div>
+              <DataTable columns={columns} data={data} />
+            </div>
+          </TabsContent>
+          <TabsContent value="investment">
+            <div className='mx-auto flex max-w-6xl flex-col items-start justify-start gap-4'>
+              <div className="w-[72rem]">
+                <p>Soon...</p>
+              </div>
+            </div>
+          </TabsContent>
+        </Tabs>
       </div>
     </div>
   )
